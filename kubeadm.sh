f1106d2cb27e13a9526b7b9eda167d00734c2de0
Shivam2135 <7768822965shivam@gmail.com>
2023-12-23 16:55:37 +0530

    Updated

--- kubeadm.sh
+++ kubeadm.sh
@@ -6,7 +6,7 @@ sudo chmod 777 /var/run/docker.sock
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo tee /etc/apt/sources.list.d/kubernetes.list <<EOF
-deb https://apt.kubernetes.io/ kubernetes-xenial main     # 3lines same command
+deb https://apt.kubernetes.io/ kubernetes-xenial main     
 EOF
 
 sudo apt-get update
